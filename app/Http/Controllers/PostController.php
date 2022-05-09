8b4539ec1b7cd59243bb9f72c6cc1d521d19d978
Zue Zue <zuezue@Zues-MBP.lan>
2022-05-09 20:40:02 +0630

    After Optimizing the query and Returning Posts list using Resource

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -5,27 +5,16 @@ namespace App\Http\Controllers;
 use App\Models\Like;
 use App\Models\Post;
 use Illuminate\Http\Request;
+use App\Http\Resources\PostResourceCollection;
+use Symfony\Component\HttpFoundation\Response;
 
 class PostController extends Controller
 {
     public function list()
     {
-        $posts = Post::get();
-        
-        $data = collect();
-        foreach ($posts as $post) {
-            $data->add([
-                'id'          => $post->id,
-                'title'       => $post->title,
-                'description' => $post->description,
-                'tags'        => $post->tags,
-                'like_counts' => $post->likes->count(),
-                'created_at'  => $post->created_at,
-            ]);
-        }
-        return response()->json([
-            'data' => $data,
-        ]);
+        $posts = Post::with('tags')->withCount('likes')->get();
+
+        return response()->json(new PostResourceCollection($posts), Response::HTTP_OK);
     }
     
     public function toggleReaction(Request $request)
